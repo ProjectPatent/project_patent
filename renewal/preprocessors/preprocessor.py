703966e97f85f55e09a35ef5bb49f861751fa246
wooyeol.park <wooyeol.dev@gmail.com>
2024-11-20 02:59:41 +0900

    Add: 새로운 형식의 raw data 전처리 모듈 추가

--- renewal/preprocessors/preprocessor.py
+++ renewal/preprocessors/preprocessor.py
@@ -2,10 +2,12 @@
 데이터 전처리하는 모듈
 '''
 
-from lxml import etree
+import json
 
-from utils.time_utils import get_today_yymmdd, is_yymmdd_format
-from config.api_config import API_OUTPUT_PARAMS_TO_PARSE
+from utils.time_utils import get_today_yyyymmdd, is_yyyymmdd_format
+from config.api_config import API_PARAMS_TO_PARSE, TABLES
+
+from db.mysql_loader import Database
 
 
 class DataParser():
@@ -13,105 +15,140 @@ class DataParser():
     XML 파일을 읽고 데이터를 전처리하는 클래스입니다.
     '''
 
-    def __init__(self, path, date=None):
-        self.path = path
+    def __init__(self, raw_data_path, output_data_path, date=None):
+        self.raw_data_path = raw_data_path
+        self.output_data_path = output_data_path
+        self.mysql_loader = Database()
 
-        if date is not None and is_yymmdd_format(date):
+        self.ipr_reg_data = {}
+        self.ipc_cpc_data = {}
+        self.priority_data = {}
+
+        if date is not None and is_yyyymmdd_format(date):
             self.date = date
         else:
-            self.date = get_today_yymmdd()
+            self.date = get_today_yyyymmdd()
 
-    def xml_to_list(self, org_type):
+    def json_to_query_values(self, org_type):
         '''
         기업, 대학 | 특허/실용신안, 디자인, 상표 xml파일을 읽어서 데이터를 리턴
-        input : service_type : patent_utility (특허/실용신안)
+        input : data_service : patuti (특허/실용신안)
                              design (디자인)
                              trademark (상표)
-                org_type : corp (기업)
+                data_class : corp (기업)
                              univ (대학)
         '''
-        self.ipr_reg_data_list = []
-        self.ipc_cpc_data_list = []
-        self.priority_data_list = []
 
-        self.ipr_reg_parser(org_type, service_type='patent_utility')
-        self.ipr_reg_parser(org_type, service_type='design')
-        self.ipr_reg_parser(org_type, service_type='trademark')
+        self.ipr_reg_parser(org_type, ipr_mode='patuti')
+        self.ipr_reg_parser(org_type, ipr_mode='design')
+        self.ipr_reg_parser(org_type, ipr_mode='trademark')
 
-        return self.ipr_reg_data_list, self.ipc_cpc_data_list, self.priority_data_list
+        open(f'{self.output_data_path}/ipr_reg_{self.date}_{org_type}_values.json', 'w',
+             encoding='utf-8').write(json.dumps(self.ipr_reg_data, ensure_ascii=False))
+        open(f'{self.output_data_path}/ipc_cpc_{self.date}_{org_type}_values.json', 'w',
+             encoding='utf-8').write(json.dumps(self.ipc_cpc_data, ensure_ascii=False))
+        open(f'{self.output_data_path}/priority_{self.date}_{org_type}_values.json', 'w',
+             encoding='utf-8').write(json.dumps(self.priority_data, ensure_ascii=False))
 
-    def ipr_reg_parser(self, org_type, service_type):
+    def ipr_reg_parser(self, org_type, ipr_mode):
         '''
-        service_type에 해당하는 파라미터에 맞춰서 클래스 변수에 저장합니다.
+        data_service에 해당하는 파라미터에 맞춰서 클래스 변수에 저장합니다.
         서브 테이블인 ipc_code, priority 함수를 호출해서 함께 저장합니다.
-        input : service_type : patent_utility (특허/실용신안)
+        input : data_service : patuti (특허/실용신안)
                                design (디자인)
                                trademark (상표)
         '''
-        path = f'{self.path}/{self.date}_{service_type}_{org_type}.xml'
-        # try:
-        tree = etree.parse(path)
-        root = tree.getroot()
-        temp = None
-        for item in root.iter('item'):
-            if service_type == 'patent_utility':
-                temp = self.ipc_cpc_parser(item)
-            elif service_type in ('design', 'trademark'):
-                temp = self.priority_parser(service_type, item)
-            if temp is not None:
-                self.ipr_reg_data_list.append(temp)
+        path = f'{self.raw_data_path}/{ipr_mode}_{self.date}_{org_type}.json'
+        ipr_data = None
+        with open(path, 'r', encoding='utf-8') as file:
+            json_data = json.load(file)
+            ipr_mode = json_data['ipr_mode']
+            self.ipr_reg_data['table_name'] = TABLES[org_type.upper()
+                                                     ]['IPR_REG'][0]
+            self.ipr_reg_data['values'] = []
+            for item in json_data['data']:
+                if item['applicationNumber'] is None:
+                    continue
+                if ipr_mode == 'patuti':
+                    ipr_data = self.ipc_cpc_parser(item, org_type, ipr_mode)
+                elif ipr_mode in ('design', 'trademark'):
+                    ipr_data = self.priority_parser(item, org_type, ipr_mode)
+                if ipr_data is not None:
+                    self.ipr_reg_data['values'].append(ipr_data)
 
-    def ipc_cpc_parser(self, item):
+    def ipc_cpc_parser(self, item, org_type, ipr_mode):
         '''
-        ipc_code가 있다면 클래스 변수 (ipc_cpc_data_list)에 저장합니다.
+        ipc_code가 있으면 클래스 변수 (ipc_cpc_data_list)에 저장합니다.
         ipr_reg_data_list에 대한 key, value값을 딕셔너리에 저장 후 리턴합니다.
         '''
 
-        temp = {}
-        for key, value in API_OUTPUT_PARAMS_TO_PARSE['patent_utility'].items():
-            if value == 'applicationNumber':
-                appl_no = str(item.find(f'.//{value}').text)
-                temp[f'{key}'] = appl_no
-                temp['ipr_code'] = appl_no[0] + '0'
-            elif value == 'ipcNumber':
-                ipc_codes = item.find(f'.//{value}').text.split('|')
-                temp[f'{key}'] = ipc_codes[0]
+        ipr_data = {}
+        ipc_codes = []
+        table_name = TABLES[org_type.upper()]['IPC_CPC'][0]
+        table_columns = TABLES[org_type.upper()]['IPC_CPC'][1].keys()
+
+        self.ipc_cpc_data['table_name'] = table_name
+        self.ipc_cpc_data['values'] = []
+
+        for column in table_columns:
+            output_param = API_PARAMS_TO_PARSE[ipr_mode][column]
+            if column == 'appl_no':
+                ipr_data[column] = item[output_param]
+            elif column == 'ipr_code':
+                ipr_data[column] = item[output_param][0:2]
+            elif column == 'main_ipc':
+                ipc_codes = item[output_param].split('|')
+                ipr_data[column] = ipc_codes[0]
+            elif column in API_PARAMS_TO_PARSE[ipr_mode]:
+                ipr_data[column] = item[output_param]
             else:
-                temp[f'{key}'] = item.find(f'.//{value}').text
-        for code in ipc_codes:
-            self.ipc_cpc_data_list.append({
-                'appl_no': temp['appl_no'],
-                'ipc_cpc': 'ipc',  # 추후 ipc, cpc 구분 필요
-                'ipc_cpc_code': code
+                ipr_data[column] = None
+        for ipc_code in ipc_codes:
+            self.ipc_cpc_data['values'].append({
+                'appl_no': ipr_data['appl_no'],
+                'ipc_cpc': 'ipc',
+                'ipc_cpc_code': ipc_code,
             })
 
-        return temp
+        return ipr_data
 
-    def priority_parser(self, service_type, item):
+    def priority_parser(self, item, org_type, ipr_mode):
         '''
         priority에 대한 데이터가 존재한다면 클래스 변수(priority_data_list)에 저장합니다.
         ipr_reg_data_list에 대한 key, value값을 딕셔너리에 저장 후 리턴합니다.
         '''
 
-        temp = {}
-        for key, value in API_OUTPUT_PARAMS_TO_PARSE[service_type].items():
-            if value == 'applicationNumber':
-                appl_no = str(item.find(f'.//{value}').text)
-                temp[f'{key}'] = appl_no
-                temp['ipr_code'] = appl_no[:1] + '0'
-            else:
-                temp[f'{key}'] = item.find(f'.//{value}').text
-
+        ipr_data = {}
         priority_data = {}
-        if temp['priority_no'] is not None:
-            priority_data['priority_no'] = temp['priority_no']
-        temp.pop('priority_no')
-        if temp['priority_date'] is not None:
-            priority_data['priority_date'] = temp['priority_date']
-        temp.pop('priority_date')
-        if len(priority_data) > 0:
-            priority_data['applicant_no'] = temp['applicant_no']
-            priority_data['appl_no'] = temp['appl_no']
-            self.priority_data_list.append(priority_data)
+        table_name = TABLES[org_type.upper()]['PRIORITY'][0]
+        table_columns = TABLES[org_type.upper()]['PRIORITY'][1].keys()
 
-        return temp
+        self.priority_data['table_name'] = table_name
+        self.priority_data['values'] = []
+
+        for column in table_columns:
+            output_param = API_PARAMS_TO_PARSE[ipr_mode][column]
+            if column == 'appl_no':
+                ipr_data[column] = item[output_param]
+            elif column == 'ipr_code':
+                ipr_data[column] = item[output_param][0:2]
+            elif column in API_PARAMS_TO_PARSE[ipr_mode]:
+                ipr_data[column] = item[output_param]
+            else:
+                ipr_data[column] = None
+
+        priority_number = item['priorityNumber']
+        priority_date = item['priorityDate']
+        if priority_number or priority_date:
+            priority_data['appl_no'] = ipr_data['appl_no']
+            try:
+                priority_data['priority_date'] = priority_date
+            except KeyError:
+                priority_data['priority_date'] = None
+            try:
+                priority_data['priority_no'] = priority_number
+            except KeyError:
+                priority_data['priority_no'] = None
+            self.priority_data['values'].append(priority_data)
+
+        return ipr_data
--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,13 +1,19 @@
 from datetime import datetime
 import pytz
 
-def get_today_yymmdd(timezone="Asia/Seoul"):
+
+def get_today_yyyymmdd(timezone="Asia/Seoul"):
     tz = pytz.timezone(timezone)
     return datetime.now(tz).strftime("%Y%m%d")
 
 
-def is_yymmdd_format(date_str):
-    # yymmdd 형식이며 유효한 날짜인지 확인
+def get_timestamp(timezone="Asia/Seoul"):
+    tz = pytz.timezone(timezone)
+    return datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+
+
+def is_yyyymmdd_format(date_str):
+    # yyyymmdd 형식이며 유효한 날짜인지 확인
     try:
         datetime.strptime(date_str, "%Y%m%d")
         return True
